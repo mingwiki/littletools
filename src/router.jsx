1fa2724b041f1604c24555600b4d11efaceb2e0d
mingwiki <mingwiki@gmail.com>
2022-04-28 17:00:18 +0800

    修改一下路由

--- src/router.jsx
+++ src/router.jsx
@@ -19,32 +19,32 @@ const Component = () => {
   const [isCollapsed, setIsCollapsed] = useState(false)
   const toggle = () => setIsCollapsed(!isCollapsed)
   return (
-    <Layout>
-      <HashRouter>
-        <Suspense fallback={<Spin size="large" style={{ position: 'fixed', top: '50%', left: '50%', transform: 'translate(-50%, -50%)' }} />} >
-          <Sider collapsible collapsed={isCollapsed} onCollapse={toggle}>
-            <div className="logo" style={{ textAlign: 'center' }}>
-              <Link to='/'>
-                <img src={logo} className="App-logo" alt="logo" />
-              </Link>
-            </div>
-            <Menu
-              theme="dark"
-              mode="inline"
-              defaultSelectedKeys={['1']}
-              items={sidebar}
-            />
-          </Sider>
-          <StyledLayout>
+    <HashRouter>
+      <Layout>
+        <Sider collapsible collapsed={isCollapsed} onCollapse={toggle}>
+          <div className="logo" style={{ textAlign: 'center' }}>
+            <Link to='/'>
+              <img src={logo} className="App-logo" alt="logo" />
+            </Link>
+          </div>
+          <Menu
+            theme="dark"
+            mode="inline"
+            defaultSelectedKeys={['1']}
+            items={sidebar}
+          />
+        </Sider>
+        <StyledLayout>
+          <Suspense fallback={<Spin size="large" style={{ position: 'fixed', top: '50%', left: '50%', transform: 'translate(-50%, -50%)' }} />} >
             <Routes>
               <Route path="/" element={<Welcome />} />
               <Route path="/AppPageToUrl" element={<AppPageToUrl />} />
             </Routes>
-            <StyledFooter>{appTitle} &copy; {new Date().getFullYear()}</StyledFooter>
-          </StyledLayout>
-        </Suspense>
-      </HashRouter>
-    </Layout>
+          </Suspense>
+          <StyledFooter>{appTitle} &copy; {new Date().getFullYear()}</StyledFooter>
+        </StyledLayout>
+      </Layout>
+    </HashRouter>
   )
 }
 export default Component
