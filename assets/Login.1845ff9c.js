1996556c57d921909682901d8ef9f6d86dcd121b
mingwiki <mingwiki@gmail.com>
2022-06-02 17:07:05 +0000

    Deploying to gh-pages from @ mingwiki/toolkits@627aa0d423bc8ffa84bd8b7b2fce826343cc2d92 🚀

---
+++ assets/Login.1845ff9c.js
@@ -0,0 +1,8 @@
+import{s as n,r as p,c,W as d,a as e,j as t,X as a}from"./index.abe404c2.js";import F from"./index.85874256.js";import{F as s}from"./index.7c5b09ce.js";import{I as i}from"./index.72e217d1.js";import{S as h,B as o}from"./index.e0741709.js";import"./index.2d9ac2c0.js";import"./col.c66e84d8.js";import"./responsiveObserve.5e05f819.js";import"./index.f633c333.js";import"./SearchOutlined.df73f75f.js";const f=n.div`
+  font-size: 36px;
+  margin: 50px 0;
+`,C=n.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+`,S=()=>{const{AuthStore:u}=p.exports.useContext(c);let m=d();return e(F,{children:t(C,{children:[e(f,{children:"\u6B22\u8FCE\u4F7F\u7528\uFF0C\u8BF7\u5148\u767B\u5F55"}),t(s,{name:"basic",labelCol:{span:8},wrapperCol:{span:16},initialValues:{remember:!0},onFinish:r=>{u.setPassword(r.password),u.setUsername(r.username),u.login().then(()=>{a.success("\u767B\u5F55\u6210\u529F!"),window.history.back()}).catch(l=>{a.error("\u767B\u5F55\u5931\u8D25:",l)})},onFinishFailed:r=>{a.error("\u767B\u5F55\u5931\u8D25:",r)},autoComplete:"off",children:[e(s.Item,{label:"\u7528\u6237\u540D",name:"username",rules:[{required:!0,message:"\u8BF7\u8F93\u5165\u7528\u6237\u540D"}],children:e(i,{})}),e(s.Item,{label:"\u5BC6\u7801",name:"password",rules:[{required:!0,message:"\u8BF7\u8F93\u5165\u5BC6\u7801!"}],children:e(i.Password,{})}),e(s.Item,{wrapperCol:{offset:8,span:16},children:t(h,{children:[e(o,{type:"primary",htmlType:"submit",children:"\u63D0\u4EA4"}),e(o,{type:"primary",onClick:()=>m("/register"),children:"\u524D\u5F80\u6CE8\u518C"})]})})]})]})})};export{S as default};
--- assets/index.7fe3bab8.js
+++
@@ -1,9 +0,0 @@
-import{r as e,a as r,s as a,d as s}from"./index.f35a7376.js";import{g as n}from"./index.2d9ac2c0.js";const{Content:p}=s,i=a(p)`
-  margin: 16px;
-  padding: 24px;
-  border: 2px dashed;
-  display: flex;
-  flex-direction: column;
-  gap: 10px;
-  overflow: auto;
-`;var x=({children:o})=>{const t=e.exports.useRef(null);return e.exports.useEffect(()=>{n.timeline().set(t.current,{x:"-100%",opacity:0}).to(t.current,{x:"0",opacity:1,ease:"ease-in-out",duration:1})},[]),r(i,{ref:t,children:o})};export{x as default};
