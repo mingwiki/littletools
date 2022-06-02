aa00a649e090f75425d7ab78efd1c0d5daa835bf
mingwiki <mingwiki@gmail.com>
2022-06-02 15:11:01 +0000

    Deploying to gh-pages from @ mingwiki/toolkits@3129b651e6f216e0f049f75669dfa8be68eb50e9 🚀

---
+++ assets/index.f4c477c0.js
@@ -0,0 +1,9 @@
+import{r as e,a,s as r,d as s}from"./index.e7328609.js";import{g as n}from"./index.2d9ac2c0.js";const{Content:p}=s,i=r(p)`
+  margin: 16px;
+  padding: 24px;
+  border: 2px dashed;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  overflow: auto;
+`;var x=({children:o})=>{const t=e.exports.useRef(null);return e.exports.useEffect(()=>{n.timeline().set(t.current,{x:"100%",opacity:0,ease:"ease-in-out",duration:1}).to(t.current,{x:"0",opacity:1,ease:"ease-in-out",duration:1})},[]),a(i,{ref:t,children:o})};export{x as default};
