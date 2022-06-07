64e3874cc6c990f07e7543ab871471544af8cc33
mingwiki <mingwiki@gmail.com>
2022-06-07 02:26:15 +0000

    Deploying to gh-pages from @ mingwiki/toolkits@693dc426d7c030997cc35a106b84561dafe86435 🚀

--- assets/LoginRequired.9861cc1b.js
+++
@@ -1,4 +0,0 @@
-import{s as n,r as t,c as a,j as e,a as i,L as s}from"./index.c324590b.js";import{C as c}from"./index.618bd646.js";const u=n.div`
-  display: flex;
-  font-size: 30px;
-`,x=()=>{const{HeaderStore:o}=t.exports.useContext(a),{setHeaders:r}=o;return t.exports.useEffect(()=>{r({ghost:!1,onBack:()=>window==null?void 0:window.history.back(),title:"",subTitle:"",extra:[]})},[]),e(c,{children:i(u,{children:["\u8BF7\u5148",e(s,{to:"/login",children:"\u767B\u5F55"}),"\u6216\u8005",e(s,{to:"/register",children:"\u6CE8\u518C"})]})})};export{x as default};
--- assets/index.9cccbdf8.js
+++
@@ -1,5 +0,0 @@
-import{s as r,r as s,j as a,b as d}from"./index.c324590b.js";const{Content:c}=d,p=r(c)`
-  margin: 16px;
-  padding: 24px;
-  border: 2px dashed;
-`,x=s.exports.forwardRef((t,e)=>{const{children:n}=t,{contentRef:o}=e;return a(p,{ref:o,children:n})});export{x as default};
