6aff3887939311db5b8dc0e1ea0300a78e9e119e
mingwiki <mingwiki@gmail.com>
2022-06-14 09:20:28 +0000

    Deploying to gh-pages from @ mingwiki/toolkits@5b7cd6ec3e8ee3df2a99b1a75d451163d9469d52 🚀

--- assets/LoginRequired.57e2d090.js
+++
@@ -1,4 +0,0 @@
-import{s as a,r as t,c as n,j as e,a1 as i,a as c,L as s}from"./index.aed5c311.js";const u=a.div`
-  display: flex;
-  font-size: 30px;
-`,d=()=>{const{HeaderStore:o}=t.exports.useContext(n),{setHeaders:r}=o;return t.exports.useEffect(()=>{r({ghost:!1,onBack:()=>window==null?void 0:window.history.back(),title:"",subTitle:"",extra:[]})},[]),e(i,{children:c(u,{children:["\u8BF7\u5148",e(s,{to:"/login",children:"\u767B\u5F55"}),"\u6216\u8005",e(s,{to:"/register",children:"\u6CE8\u518C"})]})})};export{d as default};
