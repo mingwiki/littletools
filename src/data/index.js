70d7d518060a34931946e6e0eac78fb287e40405
mingwiki <mingwiki@gmail.com>
2022-08-23 14:22:23 +0800

    增加提速链接

--- src/data/index.js
+++ src/data/index.js
@@ -33,6 +33,7 @@ export const miniAppPages = {
     商机单: 'pages/businessOrder/index',
     宽带红包卡券: 'pages/activity/bbRedActivity/index',
     订单详情: 'pages/orderDetail/index',
+    提速: 'pages/activity/speedUp/index',
   },
 }
 export const miniAppPageExtra = {
