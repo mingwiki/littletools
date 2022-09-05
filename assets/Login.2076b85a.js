6e2de58ca0958e6ba7acb524785b7df10aac75b5
mingwiki <mingwiki@gmail.com>
2022-09-05 04:51:20 +0000

    Deploying to gh-pages from @ mingwiki/toolkits@cee0d6604a88a88e40a6dddc2cd73639dc0b73e1 🚀

--- assets/Login.2076b85a.js
+++
@@ -1,8 +0,0 @@
-import{s as p,r as n,c as f,a2 as C,j as e,a3 as x,a as u,a4 as t}from"./index.ece7a5dd.js";import{F as a}from"./index.ca678bbb.js";import{I as l}from"./index.c220e0e6.js";import{S as g}from"./index.848f0d81.js";import{B as m}from"./button.da1e422b.js";import"./TextArea.649e0ccd.js";import"./index.70bff8d6.js";import"./responsiveObserve.e7ca4635.js";import"./SearchOutlined.4137eb60.js";const B=p.div`
-  font-size: 36px;
-  margin: 50px 0;
-`,y=p.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`,v=()=>{const{AuthStore:o,HeaderStore:c}=n.exports.useContext(f);let i=C();const d=r=>{o.setPassword(r.password),o.setUsername(r.username),o.login().then(s=>{Array.isArray(s)?(t.success("\u767B\u5F55\u6210\u529F!"),i("/")):t.error(s)}).catch(s=>{t.error(s)})},F=r=>{t.error(r)},{setHeaders:h}=c;return n.exports.useEffect(()=>{h({ghost:!1,onBack:()=>window==null?void 0:window.history.back(),title:"",subTitle:"",extra:[]})},[]),e(x,{children:u(y,{children:[e(B,{children:"\u6B22\u8FCE\u4F7F\u7528\uFF0C\u8BF7\u5148\u767B\u5F55"}),u(a,{name:"basic",labelCol:{span:8},wrapperCol:{span:16},initialValues:{remember:!0},onFinish:d,onFinishFailed:F,autoComplete:"on",children:[e(a.Item,{label:"\u7528\u6237\u540D",name:"username",rules:[{required:!0,message:"\u8BF7\u8F93\u5165\u7528\u6237\u540D"}],children:e(l,{})}),e(a.Item,{label:"\u5BC6\u7801",name:"password",rules:[{required:!0,message:"\u8BF7\u8F93\u5165\u5BC6\u7801!"}],children:e(l.Password,{})}),e(a.Item,{wrapperCol:{offset:8,span:16},children:u(g,{children:[e(m,{type:"primary",htmlType:"submit",children:"\u63D0\u4EA4"}),e(m,{type:"primary",onClick:()=>i("/register"),children:"\u524D\u5F80\u6CE8\u518C"})]})})]})]})})};export{v as default};
